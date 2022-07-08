e67893497eb963b52f2aa1d88d6276c52e8c49c3
Tyson Mahlangu <siphotysonmahlangu@gmail.com>
2022-07-08 11:36:04 +0200

    Add project files.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,72 @@
+﻿using ItemsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ItemsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ItemController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        [HttpGet]
+        public List<Items> GetItems()
+        {
+            return LoadList();
+        }
+        [HttpPost]
+
+        public string PostItems(Items it)
+
+        {
+            SqlConnection con = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+           SqlCommand cmd = new SqlCommand("Insert into Items Values ('" + it.ItemName + "','" + it.ItemCode + "','" + it.Price , con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            return "Added";
+
+            
+
+        }
+
+
+        private List <Items> LoadList()
+        {
+            //var items = new List<Items>();
+            List<Items> list = new List<Items>();
+
+            SqlConnection con = new
+                SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+                 con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Items ",con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Items ob = new Items();
+                ob.ItemCode = dataTable.Rows[i]["ItemCode"].ToString();
+                ob.ItemName = dataTable.Rows[i]["ItemName"].ToString();
+                ob.Price = int.Parse(dataTable.Rows[i]["Price"].ToString());
+                list.Add(ob);
+
+
+            }
+
+            return list;
+
+
+        }
+
+      
+    }
+}
---
+++ Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItemsAPI.Models
+{
+    public class Items
+    {
+        public int id { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+
+        public string ItemCode   { get; set; }
+
+        public int Price { get; set; }
+         
+    }
+}
